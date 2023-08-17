da782304deec64f20da523269d502eb83d464a59
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-08-17 23:40:02 +0530

    bufferline edits

--- lua/custom/plugins/bufferline.lua
+++ lua/custom/plugins/bufferline.lua
@@ -1,24 +1,30 @@
--- return {
---   "akinsho/bufferline.nvim",
---   version = "*",
---   dependencies = "nvim-tree/nvim-web-devicons",
---   opts = {
---     options = {
---       mode = 'tabs',
---       themable = true,
---       offsets = {
---         {filetype = 'NvimTree'}
---       },
---       highlights = {
---         buffer_selected = {
---           italic = false
---         },
---         indicator_selected = {
---           fg = {attribute = 'fg', highlight = 'Function'},
---           italic = false
---         }
---       }
---     }
---   }
--- }
-return {}
+return {
+  "akinsho/bufferline.nvim",
+  version = "*",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  opts = {
+    options = {
+      mode = 'tabs',
+      buffer_close_icon = 'x',
+      themable = false,
+      indicator = {
+        style = "underline"
+      },
+      offsets = {
+        { filetype = 'NvimTree' }
+      },
+      highlights = {
+        buffer_selected = {
+          italic = false,
+        },
+        indicator_selected = {
+          fg = { attribute = 'fg', highlight = 'Function' },
+          italic = false
+        },
+        separator_style = "slant",
+        always_show_bufferline = false,
+
+      }
+    }
+  }
+}

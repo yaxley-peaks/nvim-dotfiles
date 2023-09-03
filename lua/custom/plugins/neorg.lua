4828e03cc4800a3f3c7f3f50eb257526e8c84367
yaxley-peaks <epiclycoolgaemer@gmail.com>
2023-09-03 19:08:57 +0530

    Neorg

---
+++ lua/custom/plugins/neorg.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-neorg/neorg",
+  build = ":Neorg sync-parsers",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    require("neorg").setup {
+      load = {
+        ["core.summary"] = {},  -- autogen summary
+        ["core.defaults"] = {}, -- Loads default behaviour
+        ["core.concealer"] = {
+          config = {
+            icon_preset = "basic"
+          }
+        },                  -- Adds pretty icons to your documents
+        ["core.ui"] = {},   -- Adds pretty icons to your documents
+        ["core.dirman"] = { -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+            default_workspace = "notes"
+          },
+        },
+      },
+    }
+  end,
+}
